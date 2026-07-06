e57f5881f27be666c6d2609861b9d90041a9b4fe
Matthew Trejo <mtrejoda@fafi.utb.edu.ec>
2026-07-06 04:04:37 -0500

    Remove server-related tests and configurations

--- OsuMissAnalyzer.Server/Api/ScoreRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace OsuMissAnalyzer.Server.Api
-{
-    public class ScoreRequest
-    {
-        public ulong GuildId { get; set; }
-        public ulong ScoreId { get; set; }
-    }
-}
--- OsuMissAnalyzer.Server/Logging/DiscordLoggerConfiguration.cs
+++
@@ -1,8 +0,0 @@
-namespace OsuMissAnalyzer.Server.Logging
-{
-    public class DiscordLoggerConfiguration
-    {
-        public string? WebHook { get; set; }
-        public string? AlertPrefix { get; set; }
-    }
-}
